bbc01cd36b62276a620a3cf537485d6fbd9aa102
Tomasz Borychowski <github@borychowski.org>
2022-04-15 21:33:38 +0100

    dedupe

--- JavaScript/uuid-guid.js
+++ JavaScript/uuid-guid.js
@@ -1,5 +1,5 @@
 
-export function guid () {
+export function uuid () {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
 		const r = Math.random() * 16 | 0;
 		const v = c == 'x' ? r : (r & 0x3 | 0x8);
